ad074265f15a0248f667e4a1ba5e6ac092e67cd3
Michael Park <michael.park@canonical.com>
2026-02-24 11:46:24 +1000

    test: fix artefact version for API docs download

--- docs/jobs/pre-build-job.sh
+++ docs/jobs/pre-build-job.sh
@@ -20,22 +20,10 @@ RUN_IDS=$(gh run list \
 
 DOWNLOAD_SUCCESS=false
 
-for id in $RUN_IDS; do
-  echo -n "Checking Run ID $id... "
-  
-  gh run download "$id" -R "${REPO}" -n "${ARTIFACT_NAME}" -D "${TARGET_DIR}" >/dev/null 2>&1
-  
-  if [ $? -eq 0 ]; then
-    echo "Found and downloaded!"
-    DOWNLOAD_SUCCESS=true
-    break
-  else
-    echo "Artifact not found (skipped)."
-  fi
-done
+gh run download "22216785446" -R "${REPO}" -n "${ARTIFACT_NAME}" -D "${TARGET_DIR}" >/dev/null 2>&1
 
 if [ "$DOWNLOAD_SUCCESS" = false ]; then
-  echo "Error: Checked the last 100 successful runs, but none contained the artifact '${ARTIFACT_NAME}'."
+  echo "Error: You made a mistake. :( '${ARTIFACT_NAME}'."
   exit 1
 fi
 
